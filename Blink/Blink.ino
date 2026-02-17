fef658d05864f9d7a938a04e21968ccfbff99dca
1234cyle <cylekrohling10@gmail.com>
2026-02-17 14:15:36 -0500

    Updated lights to flash

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -28,13 +28,17 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(9, HIGH);  // turn the LED on (HIGH is the voltage level)                  
+  digitalWrite(10, LOW);   // turn the LED off by making the voltage LOW
   delay(500);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(500);                      // wait for a second
+
+  digitalWrite(10, HIGH);  // turn the LED on (HIGH is the voltage level)                   // wait for a second
+  digitalWrite(9, LOW);   // turn the LED off by making the voltage LOW
+  delay(500);
 }
